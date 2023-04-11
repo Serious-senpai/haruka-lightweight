9b80f4deb3e05d225a6b6057dddc52ee7f25ec52
Haruka3399 <57554044+Saratoga-CV6@users.noreply.github.com>
2023-04-11 08:54:42 +0700

    Added ``/urban`` command

--- bot/commands/search/urban.py
+++ bot/commands/search/urban.py
@@ -1,6 +1,7 @@
+from discord import app_commands
 from discord.ext import commands
 
-from customs import Context
+from customs import Context, Interaction
 from lib import urban
 from shared import interface
 
@@ -23,3 +24,22 @@ async def _urban_cmd(ctx: Context, *, query: str):
         await ctx.send(embed=embed)
     else:
         await ctx.send("No matching result was found.")
+
+
+@interface.slash(
+    name="urban",
+    description="Search for a term from Urban Dictionary",
+)
+@app_commands.describe(query="The searching query")
+async def _urban_slash(interaction: Interaction, query: str) -> None:
+    await interaction.response.defer(thinking=True)
+    result = await urban.UrbanSearch.search(query, session=interface.session)
+    if result:
+        embed = await result.create_embed()
+        embed.set_author(
+            name=f"{interaction.user.name} searched for {query}",
+            icon_url=interaction.user.avatar.url if interaction.user.avatar else None,
+        )
+        await interaction.followup.send(embed=embed)
+    else:
+        await interaction.followup.send("No matching result was found.")
