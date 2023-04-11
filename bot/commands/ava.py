7cd73b686846a0a6a208e85050bf5336736b33f4
Haruka3399 <57554044+Saratoga-CV6@users.noreply.github.com>
2023-04-11 10:03:34 +0700

    Added ``/ava`` command

--- bot/commands/ava.py
+++ bot/commands/ava.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
 import discord
+from discord import app_commands
 from discord.ext import commands
 
-from customs import Context
+from customs import Context, Interaction
 from shared import interface
 
 
@@ -24,3 +25,21 @@ async def _ava_cmd(ctx: Context, user: discord.User = commands.Author) -> None:
             icon_url=ctx.bot.user.avatar.url,
         )
         await ctx.send(embed=embed)
+
+
+@interface.slash(
+    name="ava",
+    description="View someone's avatar",
+)
+@app_commands.describe(user="The user to retrieve the avatar")
+async def _ava_slash(interaction: Interaction, user: discord.User) -> None:
+    if user.avatar is None:
+        await interaction.response.send_message("This user hasn't set an avatar yet!")
+    else:
+        embed = discord.Embed()
+        embed.set_image(url=user.avatar.url)
+        embed.set_author(
+            name=f"This is {user.name}'s avatar",
+            icon_url=interaction.client.user.avatar.url,
+        )
+        await interaction.response.send_message(embed=embed)
