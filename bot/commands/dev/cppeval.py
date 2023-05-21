7677d2b7a3ef382dfcd3260688a96b91dace9fa1
Haruka3399 <57554044+Saratoga-CV6@users.noreply.github.com>
2023-05-21 18:26:26 +0700

    [cppeval] Use attachment for source code

--- bot/commands/dev/cppeval.py
+++ bot/commands/dev/cppeval.py
@@ -17,58 +17,55 @@ from shared import interface
     name="cppeval",
     aliases=["cppexec"],
     brief="dev.cppeval",
-    description="Compile and run a C++ program. You can attach a text file to pipe to stdin.",
-    usage="cppeval <code> <attachment: input>",
+    description="Attach a C++ source file, compile and run.\nProgram stdin is read from the text message.",
+    usage="cppeval <stdin> <attachment>",
     hidden=True,
 )
 @commands.is_owner()
 @commands.max_concurrency(1)
-async def _handler(ctx: Context, *, code: str) -> None:
-    code = code.strip("`")
-    code = code.removeprefix("cpp")
-    code = code.removeprefix("c")
-
-    with contextlib.suppress(FileNotFoundError):
-        os.remove(C_EVAL_BINARY_PATH)
-
-    with open(C_EVAL_PATH, "w", encoding="utf-8") as writer:
-        process = await asyncio.create_subprocess_exec(
-            "g++",
-            "-std=c++2a",
-            "-Wall",
-            "-o", C_EVAL_BINARY_PATH,
-            "-x", "c++",
-            "-",
-            stdin=asyncio.subprocess.PIPE,
-            stdout=writer,
-            stderr=writer,
-        )
-
-        with utils.TimingContextManager() as measure:
-            await process.communicate(code.encode("utf-8"))
-
-    if process.returncode != 0:
-        await ctx.send(
-            f"Compiler completed with exit code {process.returncode} after {utils.format(measure.result)}",
-            file=discord.File(C_EVAL_PATH),
-        )
+async def _handler(ctx: Context, *, input: str) -> None:
+    try:
+        async with interface.session.get(ctx.message.attachments[0].url) as request:
+            code_data = await request.read()
+    except IndexError:
+        await ctx.send("Please attach a C++ source file")
     else:
+        input = input.strip("`")
+        with contextlib.suppress(FileNotFoundError):
+            os.remove(C_EVAL_BINARY_PATH)
+
         with open(C_EVAL_PATH, "w", encoding="utf-8") as writer:
-            try:
-                attachment = ctx.message.attachments[0]
-            except IndexError:
-                process = await asyncio.create_subprocess_exec(C_EVAL_BINARY_PATH, stdout=writer, stderr=writer)
-                with utils.TimingContextManager() as measure:
-                    await process.communicate()
-            else:
-                async with interface.session.get(attachment.url) as request:
-                    data = await request.read()
+            process = await asyncio.create_subprocess_exec(
+                "g++",
+                "-std=c++2a",
+                "-Wall",
+                "-o", C_EVAL_BINARY_PATH,
+                "-x", "c++",
+                "-",
+                stdin=asyncio.subprocess.PIPE,
+                stdout=writer,
+                stderr=writer,
+            )
+            with utils.TimingContextManager() as measure:
+                await process.communicate(code_data)
 
-                process = await asyncio.create_subprocess_exec(C_EVAL_BINARY_PATH, stdin=asyncio.subprocess.PIPE, stdout=writer, stderr=writer)
+        if process.returncode != 0:
+            await ctx.send(
+                f"Compiler failed with exit code {process.returncode} after {utils.format(measure.result)}",
+                file=discord.File(C_EVAL_PATH),
+            )
+        else:
+            with open(C_EVAL_PATH, "w", encoding="utf-8") as writer:
+                process = await asyncio.create_subprocess_exec(
+                    C_EVAL_BINARY_PATH,
+                    stdin=asyncio.subprocess.PIPE,
+                    stdout=writer,
+                    stderr=writer,
+                )
                 with utils.TimingContextManager() as measure:
-                    await process.communicate(data)
+                    await process.communicate(input.encode("utf-8"))
 
-        await ctx.send(
-            f"Program completed with exit code {process.returncode} after {utils.format(measure.result)}",
-            file=discord.File(C_EVAL_PATH),
-        )
+            await ctx.send(
+                f"Program completed with exit code {process.returncode} after {utils.format(measure.result)}",
+                file=discord.File(C_EVAL_PATH),
+            )
