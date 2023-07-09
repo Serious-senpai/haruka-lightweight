5934fbbc72f4e0a49ef6d1232420b42b3c1508ea
Haruka3399 <57554044+Saratoga-CV6@users.noreply.github.com>
2023-07-10 01:24:28 +0700

    Attempt to fix startup process

--- startup.sh
+++ startup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 echo "startup.sh started"
-python3 prepare.py
+python3 -m http.server $PORT &
+pid=$!
+echo "Started dummy HTTP server with process $pid"
+
+pip install -r requirements.txt
+apt install ffmpeg g++ git -y
+g++ -std=c++2a -Wall bot/c++/fuzzy.cpp -o bot/c++/fuzzy.out
+
+echo "Killing process $pid"
+kill $pid
+wait $pid
 python3 bot/main.py
