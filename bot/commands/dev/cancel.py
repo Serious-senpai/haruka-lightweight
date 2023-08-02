9481b897e4f7f82ce14aea4041cc8a57ae1a16e5
Haruka3399 <57554044+Saratoga-CV6@users.noreply.github.com>
2023-08-02 17:46:07 +0700

    Make ``cancel`` command transferable

--- bot/commands/dev/cancel.py
+++ bot/commands/dev/cancel.py
@@ -14,13 +14,16 @@ from shared import interface
     name="cancel",
     brief="dev.cancel",
     description="Cancel the running `eval` task",
+    transferable=True,
     hidden=True,
 )
 @commands.is_owner()
 async def handler(ctx: Context) -> None:
     task: Optional[asyncio.Task] = getattr(ctx.bot, EVAL_TASK_ATTR, None)
     if task is None:
-        await ctx.send("No `eval` task is running!")
+        if not await interface.transfer(ctx):
+            await ctx.send("No `eval` task is running!")
+
     else:
         task.cancel()
         await ctx.send("Task cancelled!")
