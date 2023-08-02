34cb6a84bab117dc5210bedce18457d96b28f85b
Haruka3399 <57554044+Saratoga-CV6@users.noreply.github.com>
2023-08-02 19:23:30 +0700

    Fixed bug

--- bot/commands/search/zerochan.py
+++ bot/commands/search/zerochan.py
@@ -17,24 +17,25 @@ from shared import interface
     usage="{prefix}zerochan <query>",
 )
 @commands.cooldown(1, 10, commands.BucketType.user)
-async def handler(ctx: Context, *, query: str):
+async def handler(ctx: Context, *, query: str) -> None:
     async with ctx.typing():
         urls = await zerochan.search(query, session=interface.session)
 
         if not urls:
             await ctx.send("No matching result was found.")
-        else:
-            no_results = len(urls)
-            embeds = []
-            for index, url in enumerate(urls):
-                embed = discord.Embed()
-                embed.set_author(
-                    name=f"Zerochan search for {query}",
-                    icon_url=ctx.bot.user.avatar.url,
-                )
-                embed.set_image(url=url)
-                embed.set_footer(text=f"Result {index + 1}/{no_results}")
-                embeds.append(embed)
+            return None
+
+        no_results = len(urls)
+        embeds = []
+        for index, url in enumerate(urls):
+            embed = discord.Embed()
+            embed.set_author(
+                name=f"Zerochan search for {query}",
+                icon_url=ctx.bot.user.avatar.url,
+            )
+            embed.set_image(url=url)
+            embed.set_footer(text=f"Result {index + 1}/{no_results}")
+            embeds.append(embed)
 
     display = emoji_ui.NavigatorPagination(ctx.bot, embeds)
     await display.send(ctx.channel)
